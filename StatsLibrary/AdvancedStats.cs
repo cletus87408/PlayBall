5a8ecf47941804eedb2ca73bc8b0739bf6ac48ee
Daniel Beauchaine <danbeauchaine@gmail.com>
2017-07-11 18:12:29 -0700

    Added Statistics:
    Basic Metrics:
    AB/Hr
    BB/K
    PA/SO
    TB
    RP
    SLG
    TA
    XBH
    SB%
    TC
    FP
    RF
    
    Advanced Metrics:
    RC
    BABIP
    XR
    EqA
    
    Pitching Metrics:
    ERA
    BB/9
    
    I seperated out pitching and advanced batting metrics into their own class file. If it is more appropriate to reincorporate them into one file I can do that. I finished most of the basic batting metrics. I wrote tests for every new stat including some real world data.

---
+++ StatsLibrary/AdvancedStats.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatsLibrary
+{
+    public static class AdvancedStats
+    {
+        public static double BattingAverageOnBallsInPlay(int hits, int homeRuns, int atBats, int strikeOuts, int sacFlies) //BABIP
+        {
+            int denominator = (atBats - strikeOuts - homeRuns + sacFlies);
+            return denominator != 0 ? (hits - homeRuns) / (denominator * 1.0) : 0.0;
+        }
+
+        public static double RunsCreated(int singles, int doubles, int triples, int homeRuns, int walks, int atBats, int hits) //RC
+        {
+            double totalBases = BasicStats.TotalBases(singles, doubles, triples, homeRuns);
+            int denominator = (atBats + walks);
+
+            return denominator != 0 ? ((hits + walks) * totalBases) / (1.0 * denominator) : 0.0;
+        }
+
+        public static double ExtrapolatedRuns(int singles, int doubles, int triples, int homeRuns, int hitByPitch, int walks, int intentionalWalks,int stolenBases, int caughtStealing, int atBats, int hits, int strikeouts, int groundIntoDoublePlay, int sacFly, int sacHit) // XR
+        {
+            double XR = (.50 * singles) + (.72 * doubles) + (1.04 * triples) + (1.44 * homeRuns) + (.34 * (hitByPitch + walks - intentionalWalks)) + (.25 * intentionalWalks) + (.18 * stolenBases) + (-.32 * caughtStealing) + (-.090 * (atBats - hits - strikeouts)) + (-.098 * strikeouts) + (-.37 * groundIntoDoublePlay) + (.37 * sacFly) + (.04 * sacHit);
+
+            return XR;
+        }
+
+        public static double EquivalentAverage(int hits, int singles, int doubles, int triples, int homeRuns, int walks, int hitByPitch, int stolenBases, int sacHit, int sacFly, int atBats, int caughtStealing) //EqA
+        {
+            double totalBases = BasicStats.TotalBases(singles, doubles, triples, homeRuns);
+            double denominator = (atBats + walks + hitByPitch + sacHit + sacFly + caughtStealing + (stolenBases / 3.0));
+            double EqA = ((hits + totalBases + (1.5 * (walks + hitByPitch)) + stolenBases + sacHit + sacFly) / denominator);
+
+            return denominator != 0 ? EqA : 0.0;
+        }
+    }
+}
--- StatsLibrary/BasicStats.cs
+++ StatsLibrary/BasicStats.cs
@@ -8,9 +8,84 @@ namespace StatsLibrary
 {
     public static class BasicStats
     {
-        public static double BattingAverage(int atBats, int hits)
+        //Batting
+        public static double BattingAverage(int atBats, int hits) // BA
         {
             return atBats != 0 ? hits / (1.0 * atBats) : 0.0;
+        }
+
+        public static double AtBatsPerHomeRun(int atBats, int homeRuns) // AB/HR
+        {
+            return homeRuns != 0 ? atBats / (1.0 * homeRuns) : 0.0;
+        }
+
+        public static double WalkToStrikeoutRatio(int walks, int strikeouts) // BB/K
+        {
+            return strikeouts != 0 ? walks / (1.0 * strikeouts) : 0.0; 
+        }
+
+        public static double PlateAppearancesPerStrikeout(int plateAppearances, int strikeouts) // PA/SO
+        {
+            return strikeouts != 0 ? plateAppearances / (1.0 * strikeouts) : 0.0;
+        }
+
+        public static double TotalBases(int singles, int doubles, int triples, int homeRuns) // TB
+        {
+            return (1.0 * singles + 2.0 * doubles + 3.0 * triples + 4.0 * homeRuns);
+        }
+
+        public static double RunsProduced(int runs, int runsBattedIn, int homeRuns) // RP
+        {
+            return (1.0 * runs + runsBattedIn - homeRuns);
+        }
+
+        public static double SluggingPercentage(int singles, int doubles, int triples, int homeRuns, int atBats) // SLG
+        {
+            double totalBases = BasicStats.TotalBases(singles, doubles, triples, homeRuns);
+
+            return atBats != 0 ? totalBases / (1.0 * atBats) : 0.0;
+        }
+
+        public static double TotalAverage(int singles, int doubles, int triples, int homeRuns, int hitByPitch, int walks, int stolenBases, int atBats, int hits, int caughtStealing, int groundedIntoDoublePlay) // TA
+        {
+            double totalBases = BasicStats.TotalBases(singles, doubles, triples, homeRuns);
+            int denominator = (atBats - hits + caughtStealing + groundedIntoDoublePlay);
+
+            return denominator != 0 ? (totalBases + hitByPitch + walks + stolenBases) / (1.0 * denominator) : 0.0;
+        }
+
+        public static double TimesOnBase(int hits, int walks, int hitByPitch)
+        {
+            return (1.0 * hits + walks + hitByPitch);
+        }
+
+        public static double ExtraBaseHits(int doubles, int triples, int homeRuns) // XBH
+        {
+            return (1.0 * doubles + triples + homeRuns);
+        }
+
+        //BaseRunning
+        public static double StolenBasePercentage(int stolenBases, int stolenBasesAttempted) //SB%
+        {
+            return stolenBasesAttempted != 0 ? (stolenBases / (1.0 * stolenBasesAttempted)) : 0.0;
+        }
+
+        //Fielding
+        public static double TotalChances(int assists, int putouts, int errors) //TC
+        {
+            return (assists + putouts + errors);
+        }
+
+        public static double FieldingPercentage(int assists, int putouts, int errors) //FP
+        {
+            double chances = BasicStats.TotalChances(assists, putouts, errors);
+            
+            return chances != 0 ? ((chances - errors) / (1.0 * chances)) : 0.0;
+        }
+
+        public static double RangeFactor(int putouts, int assists, double inningsPlayed) //RF
+        {
+            return inningsPlayed != 0 ? ((9.0 * (putouts + assists)) / inningsPlayed) : 0.0;
         }
     }
 }
