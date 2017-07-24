5213978ea9839044f8ca1a0ae411218ecca6c2cf
Daniel Beauchaine <danbeauchaine@gmail.com>
2017-07-24 12:27:45 -0700

    Completed the Individual portion of the Singles Plugin and added a test for it.

---
+++ LahmanStats/Singles.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using StatsLibrary;
+using StatsManager;
+using WpfApplication1;
+
+namespace LahmanStats
+{
+    // Singles plugin, Implements the IStatsPlugin interface for the
+    // Batting Average statistic using the Lahman database Entity Framework objects as the data source.
+    public class Singles : LahmanStatsBase
+    {
+        public override string Name => "Singles";
+
+        public override string ShortName => "1B";
+
+        public override string Explanation => @"Hits that advanced the batter only to first base";
+
+        public Singles(LahmanEntities db) : base(db)
+        {
+
+        }
+
+
+        private IEnumerable<IStatsAck> ComputeForIndividual(IEnumerable<string> identifiers, DateTime start, DateTime stop)
+        {
+            // For every player in the list...
+            foreach (string id in identifiers)
+            {
+                // Find the players entries in the batting database
+                var playerRows = from row in this.database.Battings where row.playerID == id select row;
+                // Find those rows from the players entries that match the years requested 
+                // Note that for the Lahman database, time granularity is by year
+                var matchingRows = from row in playerRows
+                                   where row.yearID >= start.Year
+                                   where row.yearID <= stop.Year
+                                   select row;
+
+                if (matchingRows.Any())
+                {
+                    foreach (var row in matchingRows)
+                    {
+                        StatsAck thisStat = new StatsAck { Identifier = id, Start = new DateTime(row.yearID, 1, 1), Stop = new DateTime(row.yearID, 1, 1), Target = StatsTarget.Individual };
+                        thisStat.Value = BasicStats.Singles(hits: row.H.Value, doubles: row.C2B.Value, triples: row.C3B.Value, homeRuns: row.HR.Value);
+                        yield return thisStat;
+                    }
+                }
+            }
+
+        }
+
+        private IEnumerable<IStatsAck> ComputeForTeam(IEnumerable<string> identifiers, DateTime start, DateTime stop)
+        {
+            // TODO: Not yet implemented.  Will work on later
+            return null;
+        }
+
+        private IEnumerable<IStatsAck> ComputeForLeague(IEnumerable<string> identifiers, DateTime start, DateTime stop)
+        {
+            // TODO: Not yet implemented.  Will work on later 
+            return null;
+        }
+
+        // Computes the Batting Average stat for each identifier in the incoming list (might be more than
+        // one player/team/league per invocation).
+        public override IEnumerable<IStatsAck> Compute(IEnumerable<string> identifiers, StatsTarget target, DateTime start, DateTime stop)
+        {
+            switch (target)
+            {
+                case StatsTarget.Individual: return ComputeForIndividual(identifiers, start, stop);
+                case StatsTarget.Team: return ComputeForTeam(identifiers, start, stop);
+                case StatsTarget.League: return ComputeForLeague(identifiers, start, stop);
+                default: return null;
+            }
+        }
+    }
+}
--- LahmanStatsTests/UnitTest1.cs
+++ LahmanStatsTests/UnitTest1.cs
@@ -54,5 +54,35 @@ namespace LahmanStatsTests
             Assert.AreEqual(retVals[0].Target, StatsTarget.Individual);
             Assert.AreEqual(retVals[1].Target, StatsTarget.Individual);
         }
+
+        [TestMethod]
+        public void TestSingleIndividualSingles()
+        {
+            Singles s = new Singles(database);
+
+            var retVal = s.Compute(new List<string> { "suzukic01" }, StatsTarget.Individual, new DateTime(2005, 1, 1),
+                new DateTime(2006, 1, 1));
+
+            var retVals = retVal.ToArray();
+
+            // Should be one result per year requested (2005, 2006)
+            Assert.AreEqual(retVals.Length, 2);
+
+            // Note relaxed spec on equality.  Just checking BA meets expectation to within 3 digits.
+            // Good enough to prove that I'm pulling the correct data for the given years
+            Assert.AreEqual(retVals[0].Value, 158.0, 1E-3);
+            Assert.AreEqual(retVals[1].Value, 186.0, 1E-3);
+
+            Assert.AreEqual(retVals[0].Identifier, "suzukic01");
+            Assert.AreEqual(retVals[1].Identifier, "suzukic01");
+
+            Assert.AreEqual(retVals[0].Start, new DateTime(2005, 1, 1));
+            Assert.AreEqual(retVals[0].Stop, new DateTime(2005, 1, 1));
+            Assert.AreEqual(retVals[1].Start, new DateTime(2006, 1, 1));
+            Assert.AreEqual(retVals[1].Stop, new DateTime(2006, 1, 1));
+
+            Assert.AreEqual(retVals[0].Target, StatsTarget.Individual);
+            Assert.AreEqual(retVals[1].Target, StatsTarget.Individual);
+        }
     }
 }
