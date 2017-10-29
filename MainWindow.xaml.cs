8a9a8b1987e9814a6ec005c47acc1bff7e22e2b0
Bob Beauchaine <b.beauchaine@comcast.net>
2017-10-29 16:04:00 -0700

    Very first cut at the main window that will drive the entry into the program features.  Added image buttons for fielding, hitting, scoring, and team management.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,49 +25,5 @@ namespace WpfApplication1
         {
             InitializeComponent();
         }
-
-        private void SearchButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            using (var db = new LahmanEntities())
-            {
-                // Find player ID by name in the master database
-                var id = (from player in db.Masters
-                          where player.nameLast == this.lastNameTextBox.Text
-                          where (player.nameGiven == this.firstNameTextBox.Text || player.nameFirst == this.firstNameTextBox.Text)
-                          orderby player.nameLast
-                          select player.playerID);
-
-                if (id.Any())
-                {
-                    int year = Convert.ToInt16(this.yearTextBox.Text);
-                    ;
-                    var playerId = id.First();
-
-                    //Player was found.  See if he has any stats for the given year
-                    var stats = from battingStats in db.Battings where battingStats.playerID == playerId where battingStats.yearID == year select battingStats;
-
-                    if (stats.Any())
-                    {
-                        // At least one entry found
-                        int atBats = 0, hits = 0;
-
-                        // Accumulate every entry in case the individual played for more than a single team
-                        // during the year in question
-                        foreach (var entry in stats)
-                        {
-                            if (entry.AB != null && entry.H != null)
-                            {
-                                atBats += entry.AB.Value;
-                                hits += entry.H.Value;
-                            }
-                        }
-
-                        this.abLabel.Content = atBats;
-                        this.hitsLabel.Content = hits;
-                        this.baLabel.Content = $"{BasicStats.BattingAverage(atBats, hits):#.###}";
-                    }
-                }
-            }
-        }
     }
 }
